b45c5cf0e4673c9a2e321ffbf4cc5b0dfd8817b6
Ace <kronicrespawn@gmail.com>
2023-05-19 13:48:02 -0500

    style commented out

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './styles/Foooter.css';
+import './styles/Footer.css';
 
 function Footer() {
   return (
     <div className="footer">
-      
+      <p> This is my footer</p>
     </div>
   );
 }
