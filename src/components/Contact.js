2d5e50aa2306f5272a4c9eed84aa75c4c86c0133
Ace <kronicrespawn@gmail.com>
2023-05-23 12:52:19 -0500

    contact form added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './styles/Contact.css';
+import ContactForm from './Form';
 
 function Contact() {
   return (
     <div className='contact'>
       <h1> contact</h1>
-      
+      <ContactForm />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -6,12 +6,46 @@ function ContactForm() {
     const onSubmit = (e) => {
         e.preventDefault()
         setForm('Submitting...')
-        const {name, email, message}
+        const {name, email, message} = e.target.elements
+        let contact_form = {
+          name: name.value,
+          email: email.value,
+          message: message.value
+        }
+        console.log(contact_form)
     }
 
 
   return (
-    <div>
+    <div className='container mt-5'>
+      <h2 className='mb-3'> Contact Form Test</h2>
+      <form onSubmit={onSubmit}>
+        <div className='mb-3'>
+          <label className='form-label' htmlFor='name'>
+            Name
+          </label>
+          <input className='form-control' type='text' id='name' required />
+        </div>
+
+        <div className='mb-3'>
+          <label className='form-label' htmlFor='email'>
+            email
+          </label>
+          <input className='form-control' type='email' id='email' required />
+        </div>
+
+        <div className='mb-3'>
+          <label className='form-label' htmlFor='message'>
+            message
+          </label>
+          <textarea className='form-control' type='message' id='message' required />
+        </div>
+
+        <button className="btn btn-danger"  type='submit' >
+          {form}
+        </button>
+
+      </form>
       
     </div>
   );
