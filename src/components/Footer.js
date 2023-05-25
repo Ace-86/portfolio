8229e725a1c4ec4e7b32ef42a9a3f2d4e1aecee0
Ace <kronicrespawn@gmail.com>
2023-05-24 23:09:39 -0500

    style updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './styles/Footer.css';
+// import React from 'react';
+// import './styles/Footer.css';
 
-function Footer() {
+// function Footer() {
+//   return (
+//     <div className="footer">
+//       <p> This is my footer</p>
+//     </div>
+//   );
+// }
+
+// export default Footer;
+// components/Footer.js
+
+import React from 'react';
+
+const Footer = () => {
   return (
-    <div className="footer">
-      <p> This is my footer</p>
-    </div>
+    <footer>
+      <div className="footer-content">
+        <p>&copy; 2023 Your Name</p>
+      </div>
+    </footer>
   );
-}
+};
 
 export default Footer;
