c97d296d7a36bca3b63a6c2b1518e46360dfbf2d
Ace <kronicrespawn@gmail.com>
2023-05-25 21:50:13 -0500

    scrolltosection fixed

--- src/App.js
+++ src/App.js
@@ -1,92 +1,38 @@
-// import {  Element, scrollToTop } from 'react-scroll';
-// import Footer from './components/Footer';
-// import Hero from './components/Hero';
-// import NavBar from './components/NavBar';
-// import About from './components/About';
-// import Project from './components/Project';
-// import Contact from './components/Contact';
-// import React from 'react';
-// import "./App.css";
-
-// const styles = {
-//   fontFamily: 'sans-serif',
-//   textAlign: 'center',
-// };
-
-    
-
-
-
-
-
-
-// function App () {
-//   const handleScrollToTop = () => {
-//     window.scrollTo({top: 0, behavior: 'smooth'}); 
-//   };
-
-//   return (
-//       <>
-//       <NavBar />
-//       <Element name="Hero" className="element hero-main">
-//   <Hero />
-// </Element>
-// <div className="wavy-separator"></div>
-
-// <Element name="About" className="element about-main">
-//   <About className='none' />
-// </Element>
-// <div className="wavy-separator"></div>
-
-// <Element name="Project" className="element project-main">
-//   <Project />
-// </Element>
-// <div className="diagonal-separator"></div>
-
-// <Element name="Contact" className="element contact-main">
-//   <Contact />
-// </Element>
-//         <Footer>
-//         </Footer>
-//           <div>
-//           <a className="hello" onClick={handleScrollToTop}>To the top!</a>
-//             click here
-//           </div>
-//     </>
-
-    
-//     );
-//   }
-// ;
-
-
-// export default App;
-
-
-import React from 'react';
 import './App.css';
-
-// Components
-import Navbar from './components/NavBar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import React from 'react';
+import Navbar from './components/NavBar';
+import ProjectCarousel from './components/Project';
 
-function App() {
+const App = () => {
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <div className="App">
-      <Navbar />
-      
-      <Hero />
-      <About />
-      <section class="container">
-  <div class="wave"></div>
-</section>
+    <div>
+      <Navbar scrollToSection={scrollToSection} />
+      <section id="hero">
+        <Hero />
+      </section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="project">
+        <ProjectCarousel />
+      </section>
+      <section id="contact">
       <Contact />
-      <Footer />
+      </section>
+        <Footer />
     </div>
   );
-}
+};
 
 export default App;
