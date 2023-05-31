fd8b123c8510e1b3478e2e8cc117c43c81d15d47
Ace <kronicrespawn@gmail.com>
2023-05-31 02:20:42 -0500

    moved icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import './styles/Footer.css';
+import github from './images/github.png';
+import linked from './images/linked.png';
+import twitter from './images/twitter.png';
+
 
 const Footer = () => {
   return (
     <footer>
+        <div className='return_top'>
+          ^
+        </div>
+        
+            <div className='links-lined'>
+      <img
+        className=''
+        src={twitter}
+        alt='twitter'
+      />
+      <img
+        className=''
+        src={linked}
+        alt='linkedin'
+      />
+      <img
+        className=''
+        src={github}
+        alt='github'
+        />
+      </div>
+        
       <div className="footer-content">
         <p>&copy; 2023 Aaron Griffin</p>
       </div>
