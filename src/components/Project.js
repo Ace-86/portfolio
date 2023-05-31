85752f6fa763d6983628ca05cc0ffa323688bafc
Ace <kronicrespawn@gmail.com>
2023-05-31 02:21:14 -0500

    updated dummy content

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,77 +1,58 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./styles/Project.css";
-
 
 const projects = [
   {
     id: 1,
-    title: 'Project 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan mauris id fermentum dictum.',
-    technologies: 'HTML, CSS, JavaScript',
+    title: 'PowerGrade',
+    description: 'A grading app that pulls grades from a server and allows students to "Edit" their grade.',
+    technologies: 'React, Node.js, mysql, express',
     image: require('./images/placeholder1.png')
   },
   {
     id: 2,
-    title: 'Project 2',
-    description: 'Sed non urna. Phasellus eu ligula. Vestibulum sit amet purus.',
-    technologies: 'React.js, Node.js',
+    title: 'Reviewz',
+    description: 'Movie review app that allows edits, inputs and deletion to a database',
+    technologies: 'React.js, Node.js, mysql',
     image: require('./images/placeholder4.jpg')
   },
   {
-  id: 3,
-  title: 'Project 3',
-  description: 'Sed non urna. Phasellus eu ligula. Vestibulum sit amet purus.',
-  technologies: 'React.js, Node.js',
-  image: require('./images/placeholder3.png')
-},
-{
-  id: 4,
-  title: 'Project 4',
-  description: 'Sed non urna. Phasellus eu ligula. Vestibulum sit amet purus.',
-  technologies: 'React.js, Node.js',
-  image: require('./images/placeholder5.png')
-},
- 
+    id: 3,
+    title: 'Tetris Clone',
+    description: 'Wanted to re-create my favorite childhood game using React.js',
+    technologies: 'React.js, Javascript, Bootstrap',
+    image: require('./images/placeholder3.png')
+  },
 ];
 
-const ProjectCarousel = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const handlePrevious = () => {
-    setActiveIndex(prevIndex => (prevIndex === 0 ? projects.length - 1 : prevIndex - 1));
-  };
-
-  const handleNext = () => {
-    setActiveIndex(prevIndex => (prevIndex === projects.length - 1 ? 0 : prevIndex + 1));
-  };
-
-  const renderProject = () => {
-    const project = projects[activeIndex];
-    return (
-      <div className="project">
-        <img src={project.image} alt={project.title} />
-        <div className="project-overlay">
-          <h2>{project.title}</h2>
-          <p>{project.description}</p>
-          <p>Technologies: {project.technologies}</p>
-        </div>
-      </div>
-    );
-  };
-
+const ProjectCard = ({ project }) => {
   return (
-    
-    // <section id='project' >
-    <div className="project-carousel">
-      <button className="previous-button" onClick={handlePrevious}>
-        Previous
-      </button>
-      {renderProject()}
-      <button className="next-button" onClick={handleNext}>
-        Next
-      </button>
+    <div className="project">
+      <img src={project.image} alt={project.title} />
+      <div className="project-overlay">
+        <h2>{project.title}</h2>
+        <p>{project.description}</p>
+        <p>Technologies: {project.technologies}</p>
+      </div>
     </div>
-    // </section>
+  );
+};
+
+const ProjectContainer = ({ projects }) => {
+  return (
+    <div className="project-container">
+      {projects.map((project) => (
+        <ProjectCard key={project.id} project={project} />
+      ))}
+    </div>
+  );
+};
+
+const ProjectCarousel = () => {
+  return (
+    <section id="project">
+      <ProjectContainer projects={projects} />
+    </section>
   );
 };
 
